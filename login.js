676c0154eee827d18b0f731f69040a84493c5ccd
THANHTRUONG06082006-VN <thanh.2474802010361@vanlanguni.vn>
2026-09-17 13:37:08 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function checkLogin(username, password) {
-  if (username === 'admin' && password === '') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
   return false;
